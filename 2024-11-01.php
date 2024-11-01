12bbd8305d75f72d5dbca62572b8524c6f2c8197
Giannis Gasteratos <igasteratos@gmail.com>
2024-11-01 17:27:12 +0200

    Create 2024-11-01.php

---
+++ 2024-11-01.php
@@ -0,0 +1,18 @@
+<?php
+
+// https://www.php.net/manual/en/function.date.php
+date("Y-m-d H:i:s");
+
+// Συγκριτικοί Τελέστές: 
+// https://www.php.net/manual/en/language.operators.comparison.php
+// ==, !=, >, <, >=, <=
+
+
+$x = 50;
+if ($x == 20) {
+    echo 'Alpha';
+} else if ($x > 50) {
+    echo 'Beta';
+} else {
+    echo 'Gamma';
+}
